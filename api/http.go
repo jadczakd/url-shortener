8c004499153d7a9314868a351a0e338a106d8ab2
Daniel Jadczak <jadczak.d@gmail.com>
2020-04-03 01:57:28 +0200

    feat: api layer

--- api/http.go
+++ api/http.go
@@ -3,22 +3,71 @@ package api
 import (
 	"net/http"
 
-	"github.com/jadczakd/url-shortener/shortener" 
-)
+	"github.com/jadczakd/url-shortener/shortener"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-// "github.com/go-chi/chi"
-// jsonS "github.com/jadczakd/url-shortener/serializer/json"
-// msgPackS "github.com/jadczakd/url-shortener/serializer/msgpack"
+	"github.com/go-chi/chi"
+	jsonS "github.com/jadczakd/url-shortener/serializer/json"
+	msgPackS "github.com/jadczakd/url-shortener/serializer/msgpack"
+	"github.com/pkg/errors"
+)
 
 type RedirectInterface interface {
 	Get(http.ResponseWriter, *http.Request)
 	Post(http.ResponseWriter, *http.Request)
 }
 
-type handlers struct {
+type handler struct {
 	redirectService shortener.RedirectService
 }
 
 func NewHandler(rs shortener.RedirectService) RedirectRepo {
 	return &handler{redirectService: rs}
+}
+
+func setupRespons(w http.ResponseWriter, contentType string, body []byte, statusCode int) {
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(statusCode)
+	_, err := w.Write(body)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (h *handler) serializer(contentType string) shortener.RedirectSerializer {
+	if contentType == "application/x-msgpack" {
+		return &ms.Redirect{}
+	}
+	return &js.Redirect{}
+}
+
+func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
+	code := chi.URLParam(r, "code")
+	redirect, err := h.redirectService.Find(code)
+	if err != nil {
+		if errors.Cause(err) == shortener.ErrRedirectNotFound {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, redirect.URL, http.StatusMovedPermanently)
+}
+
+func (h *handler) Post(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("Content-Type")
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	redirect, err := h.serializer(contentType).Decode(requestBody)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	setupRespons(w, contentType, responseBody, http.StatusCreated)
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/jadczakd/url-shortener
 go 1.14
 
 require (
+	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/go-redis/redis/v7 v7.2.0
 	github.com/pkg/errors v0.9.1
-	github.com/tensor-programming/hex-microservice v0.0.0-20191024074311-1ffbdc8a3813 // indirect
+	github.com/tensor-programming/hex-microservice v0.0.0-20191024074311-1ffbdc8a3813
 	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
 	github.com/vmihailenco/msgpack v4.0.4+incompatible
 	go.mongodb.org/mongo-driver v1.3.1
